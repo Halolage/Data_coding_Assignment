1d2bd300fdd4aec4aef05cbb11695054f3bc0157
Halolage <soweiheng@gmail.com>
2023-08-09 00:44:19 +0800

    fix model

--- bookstore-inventory/src/main/java/com/bookstore/model/Book.java
+++ bookstore-inventory/src/main/java/com/bookstore/model/Book.java
@@ -15,8 +15,30 @@ public class Book {
     private String author;
     private int quantity;
 
-    // Getters, setters, constructors
-    // Getter and Setter for quantity
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public int getQuantity() {
         return quantity;
     }
